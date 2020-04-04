1f6bf6b3fe8a0a1e78d82eb0eea402f2c0ca7062
FoamyGuy <foamyguy@gmail.com>
2020-04-03 19:22:19 -0500

    adding calibration script for compass example and update compass to use calibration values.

--- examples/lis2mdl_compass.py
+++ examples/lis2mdl_compass.py
@@ -1,6 +1,7 @@
-""" Display compass heading data five times per second """
+""" Display compass heading data from a calibrated magnetometer """
+
 import time
-from math import atan2, degrees
+import math
 import board
 import busio
 import adafruit_lis2mdl
@@ -8,19 +9,31 @@ import adafruit_lis2mdl
 i2c = busio.I2C(board.SCL, board.SDA)
 sensor = adafruit_lis2mdl.LIS2MDL(i2c)
 
+# You will need the calibration values from your magnetometer calibration
+# these values are in uT and are in X, Y, Z order (min and max values)
+hardiron_calibration = [[-61.4879, 34.4782], [-43.6714, 53.5662], [-40.7337, 52.4554]]
 
-def vector_2_degrees(x, y):
-    angle = degrees(atan2(y, x))
-    if angle < 0:
-        angle += 360
-    return angle
-
-
-def get_heading(_sensor):
-    magnet_x, magnet_y, _ = _sensor.magnetic
-    return vector_2_degrees(magnet_x, magnet_y)
+# This will take the magnetometer values, adjust them with the calibraitons
+# and return a new array with the XYZ values ranging from -100 to 100
+def normalize(magvals):
+    ret = [0, 0, 0]
+    for i, axis in enumerate(magvals):
+        minv, maxv = hardiron_calibration[i]
+        axis = min(max(minv, axis), maxv)  # keep within min/max calibration
+        ret[i] =(axis - minv) * 200 / (maxv - minv) + -100
+    return ret
 
 
 while True:
-    print("heading: {:.2f} degrees".format(get_heading(sensor)))
-    time.sleep(0.2)
+    magvals = sensor.magnetic
+    normvals = normalize(magvals)
+    print("magnetometer: %s -> %s" % (magvals, normvals))
+
+    # we will only use X and Y for the compass calculations, so hold it level!
+    compass_heading = int(math.atan2(normvals[1], normvals[0]) * 180.0 / math.pi)
+    # compass_heading is between -180 and +180 since atan2 returns -pi to +pi
+    # this translates it to be between 0 and 360
+    compass_heading += 180
+
+    print("Heading:", compass_heading)
+    time.sleep(0.1)
