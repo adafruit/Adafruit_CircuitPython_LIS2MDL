48439d3824dbf24ea088329addd8b9f767d84d22
dherrada <dylan.herrada@adafruit.com>
2021-01-13 14:06:31 -0500

    Ran pre-commit, added licenses

--- adafruit_lis2mdl.py
+++ adafruit_lis2mdl.py
@@ -1,24 +1,7 @@
-# The MIT License (MIT)
+# SPDX-FileCopyrightText: 2019 Bryan Siepert for Adafruit Industries
 #
-# Copyright (c) 2019 Bryan Siepert for Adafruit Industries
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
+# SPDX-License-Identifier: MIT
+
 """
 `adafruit_lis2mdl`
 ====================================================
@@ -232,7 +215,7 @@ class LIS2MDL:  # pylint: disable=too-many-instance-attributes
     @property
     def faults(self):
         """A tuple representing interrupts on each axis in a positive and negative direction
-            ``(x_hi, y_hi, z_hi, x_low, y_low, z_low, int_triggered)``"""
+        ``(x_hi, y_hi, z_hi, x_low, y_low, z_low, int_triggered)``"""
         int_status = self._int_source
         x_hi = (int_status & 0b10000000) > 0
         y_hi = int_status & 0b01000000 > 0
--- examples/lis2mdl_compass.py
+++ examples/lis2mdl_compass.py
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
 """ Display compass heading data from a calibrated magnetometer """
 
 import time
